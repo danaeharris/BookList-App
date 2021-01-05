09bc00d1e890faf60b831fe0d2d67093379355c4
danaeharris <rvdbinns@gmail.com>
2021-01-05 14:11:37 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php'],
 
     theme: {
+        // colors: {
+        //     primary: "#FF6161",
+        //     primaryLight: "#FF9494",
+        //     lightGray: "#F4F3F3",
+        //     mediumGray: "#ECE9E9",
+        //     darkGray: "#C4C4C4",
+        //   },
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
