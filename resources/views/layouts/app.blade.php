fe9ab19e56eec5bd2fbe9cd0821f2c5381978e00
danaeharris <rvdbinns@gmail.com>
2021-01-05 14:10:12 -0500

    Simplifying the header and dashboard.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,16 +21,31 @@
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
+            <header>
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
             </header>
-
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script>
+
+            //When you type in the search input, update the search link.
+            $( ".search" ).on("input", function() {
+                $('.search-link').attr("href", "/search?q=" + $(this).val());
+            });
+
+            //When you hit the enter key on the searchbar, click the search link.
+            $(".search").on('keypress',function(e) {
+                if(e.which == 13) {
+                    $('.search-link')[0].click();
+                }
+            });
+    
+        </script>
     </body>
 </html>
